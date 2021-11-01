48c5230f4a9bfe32e81cababd9e2206ae6d9a8e0
JeffXJin <JeffXJin@gmail.com>
2021-11-01 10:41:05 -0700

    Added wireframe and navigation

--- app/src/main/java/ca/bcit/comp3717assignment2/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717assignment2/MainActivity.java
@@ -2,7 +2,9 @@ package ca.bcit.comp3717assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAgeClick(View v) {
+        Intent intent = new Intent(this, AgeCasesActivity.class);
+        startActivity(intent);
+    }
+
+    public void onDateClick(View v) {
+        Intent intent = new Intent(this, DateCasesActivity.class);
+        startActivity(intent);
+    }
+
+    public void onHealthClick(View v) {
+        Intent intent = new Intent(this, HealthCasesActivity.class);
+        startActivity(intent);
+    }
+
+    public void onGenderClick(View v) {
+        Intent intent = new Intent(this, GenderCasesActivity.class);
+        startActivity(intent);
     }
 }
