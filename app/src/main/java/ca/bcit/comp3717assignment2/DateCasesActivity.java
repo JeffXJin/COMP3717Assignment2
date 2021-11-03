1981139a705af751ffd3c1c9dc3b7a9d1df0a1bc
JeffXJin <JeffXJin@gmail.com>
2021-11-03 16:31:43 -0700

    Added filter for cases by date (month and year), takes a while to load after pressing filter button

--- app/src/main/java/ca/bcit/comp3717assignment2/DateCasesActivity.java
+++ app/src/main/java/ca/bcit/comp3717assignment2/DateCasesActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +22,11 @@ public class DateCasesActivity extends AppCompatActivity {
     List<Person> personsList;
     ListView lvPersons;
 
+    EditText monthEditText;
+    EditText yearEditText;
+    String monthString;
+    String yearString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +37,40 @@ public class DateCasesActivity extends AppCompatActivity {
         personsList = new ArrayList<Person>();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        databasePersons.addValueEventListener(new ValueEventListener() {
+//
+//            /**
+//             * Everytime there is a change in the list, the list will be uploaded
+//             * Method calls and clears list
+//             * @param dataSnapshot as an DataSnapshot
+//             */
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+////                personsList.clear();
+//                for (DataSnapshot personSnapshot : dataSnapshot.getChildren()) {
+//                    Person person = personSnapshot.getValue(Person.class);
+//                    personsList.add(person);
+//                }
+//
+//                PersonListAdapter adapter = new PersonListAdapter(DateCasesActivity.this, personsList);
+//                lvPersons.setAdapter(adapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) { }
+//        });
+//    }
+
+    public void onFilterDate(View view) {
+        monthEditText = findViewById(R.id.date_month);
+        yearEditText = findViewById(R.id.date_year);
+
+        monthString = monthEditText.getText().toString();
+        yearString = yearEditText.getText().toString();
+
         databasePersons.addValueEventListener(new ValueEventListener() {
 
             /**
@@ -45,7 +83,13 @@ public class DateCasesActivity extends AppCompatActivity {
 //                personsList.clear();
                 for (DataSnapshot personSnapshot : dataSnapshot.getChildren()) {
                     Person person = personSnapshot.getValue(Person.class);
-                    personsList.add(person);
+
+                    assert person != null;
+                    String dateYear = person.Reported_Date.substring(0,4);
+                    String dateMonth = person.Reported_Date.substring(5,7);
+                    if (dateMonth.equals(monthString) && dateYear.equals(yearString)) {
+                        personsList.add(person);
+                    }
                 }
 
                 PersonListAdapter adapter = new PersonListAdapter(DateCasesActivity.this, personsList);
